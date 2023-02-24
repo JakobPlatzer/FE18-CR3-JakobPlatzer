8ada7b7bb4a7a24776d135036f0d94766af2e5a1
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-24 12:43:27 +0100

    menu works

--- FE18-CodeReview3-JakobPlatzer/src/app/menu/menu.component.ts
+++ FE18-CodeReview3-JakobPlatzer/src/app/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { menuItems } from '../menuItems';
+import { Meal } from '../menuItems';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  menuItems: Array<Meal> = menuItems;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
