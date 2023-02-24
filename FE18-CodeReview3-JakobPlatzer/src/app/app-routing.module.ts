b8f16bd71104e247b65797a6c191c55cea1e523d
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-24 13:41:49 +0100

    details function start

--- FE18-CodeReview3-JakobPlatzer/src/app/app-routing.module.ts
+++ FE18-CodeReview3-JakobPlatzer/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
-    path:"", component: HomeComponent
+    path:"",component: HomeComponent
   },{
     path:"menu",component: MenuComponent
+  },{
+    path: "aboutUs",component: AboutusComponent
+  },{
+    path: 'menuItems/:itemId',component: DetailsComponent
   }
 ];
 
--- FE18-CodeReview3-JakobPlatzer/src/app/details/details.component.ts
+++ FE18-CodeReview3-JakobPlatzer/src/app/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { menuItems, Meal } from '../menuItems';
 
 @Component({
   selector: 'app-details',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class DetailsComponent {
 
+  item: Meal = {} as Meal;
+  id: number = 0;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params['itemId'];
+      this.item = menuItems[this.id];
+    });
+
+  }
 }
