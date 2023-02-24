670f03b5f7222b46a68955ee6c90e4a0f47ee4f4
Jakob Platzer <jakob.platzer@gmx.at>
2023-02-24 11:54:04 +0100

    added the itemlist+interface

---
+++ FE18-CodeReview3-JakobPlatzer/src/app/menuItems.ts
@@ -0,0 +1,71 @@
+export const menuItems = [
+    {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, {
+        name: "Pizza Margherita",
+        image: "https://cdn.pixabay.com/photo/2020/01/27/12/56/cheese-4797173_1280.jpg",
+        description: "cheese, tomato, oregano",
+        price: 9,
+        vfactor: "vegetarian"
+    }, 
+]
+
+export interface Meal {
+    name: string,
+    image: string,
+    description: string,
+    price: number,
+    vfactor?: string
+}
